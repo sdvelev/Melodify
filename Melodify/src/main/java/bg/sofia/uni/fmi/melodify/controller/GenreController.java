f839c76ca83d8ee343d92a9843416f9d6cede287
Stefan Velev <sdvelev@gmail.com>
2023-12-27 00:45:32 +0200

    Added service and controller classes for the genre relation

---
+++ Melodify/src/main/java/bg/sofia/uni/fmi/melodify/controller/GenreController.java
@@ -0,0 +1,89 @@
+package bg.sofia.uni.fmi.melodify.controller;
+
+import bg.sofia.uni.fmi.melodify.dto.GenreDto;
+import bg.sofia.uni.fmi.melodify.mapper.GenreMapper;
+import bg.sofia.uni.fmi.melodify.model.Genre;
+import bg.sofia.uni.fmi.melodify.service.GenreService;
+import bg.sofia.uni.fmi.melodify.validation.ApiBadRequest;
+import bg.sofia.uni.fmi.melodify.validation.ResourceNotFoundException;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/genres")
+@Validated
+public class GenreController {
+
+    private final GenreService genreService;
+    private final GenreMapper genreMapper;
+
+    @Autowired
+    public GenreController(GenreService genreService, GenreMapper genreMapper) {
+        this.genreService = genreService;
+        this.genreMapper = genreMapper;
+    }
+
+    @GetMapping
+    public List<GenreDto> getAllGenres() {
+        return genreMapper.toDtoCollection(genreService.getAllGenres());
+    }
+
+    @GetMapping(value = "/{id}")
+    public GenreDto getGenreById(@PathVariable
+                                           @NotNull(message = "The provided genre id cannot be null")
+                                           @Positive(message = "The provided genre id must be positive")
+                                     Long id) {
+        Optional<Genre> potentialGenreToReturn = genreService.getGenreById(id);
+        if (potentialGenreToReturn.isPresent()) {
+            return genreMapper.toDto(potentialGenreToReturn.get());
+        }
+
+        throw new ResourceNotFoundException("There is not a genre with such an id");
+    }
+
+    @PostMapping
+    public Long addGenre(@NotNull(message = "The provided genre description in the body cannot be null")
+                         @RequestBody
+                             GenreDto genreDto) {
+        Genre potentialGenreToCreate = genreService.createGenre(genreMapper.toEntity(genreDto));
+
+        if (potentialGenreToCreate == null) {
+            throw new ApiBadRequest("There was a problem in creating a genre");
+        }
+
+        return potentialGenreToCreate.getId();
+    }
+
+    @DeleteMapping(params = {"genre_id"})
+    public GenreDto deleteGenreById(@RequestParam("genre_id")
+                               @NotNull(message = "The provided genre id cannot be null")
+                               @Positive(message = "The provided genre id must be positive")
+                               Long genreId) {
+        return genreMapper.toDto(genreService.deleteGenre(genreId));
+    }
+
+    @PutMapping(value = "/{id}")
+    public boolean setGenreById(@PathVariable
+                                    @NotNull(message = "The provided genre id cannot be null")
+                                    @Positive(message = "The provided genre id must be positive")
+                                    Long id,
+                                @RequestBody
+                                @NotNull(message = "The provided genre dto in the body of the query cannot be null")
+                                GenreDto genreToUpdate) {
+        return genreService.setGenreById(genreToUpdate, id);
+    }
+}
---
+++ Melodify/src/main/java/bg/sofia/uni/fmi/melodify/service/GenreService.java
@@ -0,0 +1,82 @@
+package bg.sofia.uni.fmi.melodify.service;
+
+import bg.sofia.uni.fmi.melodify.dto.GenreDto;
+import bg.sofia.uni.fmi.melodify.model.Genre;
+import bg.sofia.uni.fmi.melodify.repository.GenreRepository;
+import bg.sofia.uni.fmi.melodify.validation.ResourceNotFoundException;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Validated
+public class GenreService {
+
+    private final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    public Genre createGenre(@NotNull(message = "The provided genre cannot be null") Genre genreToSave) {
+        return genreRepository.save(genreToSave);
+    }
+
+    public List<Genre> getAllGenres() {
+        return genreRepository.findAll();
+    }
+
+    public Optional<Genre> getGenreById(
+        @NotNull(message = "The provided id cannot be null")
+        @Positive(message = "The provided id must be positive")
+        Long id) {
+        Optional<Genre> potentialGenre = genreRepository.findById(id);
+
+        if (potentialGenre.isPresent()) {
+            return potentialGenre;
+        }
+
+        throw new ResourceNotFoundException("There is not a genre with such an id");
+    }
+
+    public boolean setGenreById(
+        @NotNull(message = "The provided genre description cannot be null")
+        GenreDto genreDtoToChange,
+        @NotNull(message = "The provided genre id cannot be null")
+        @Positive(message = "The provided genre id must be positive")
+        Long genreId) {
+
+        Optional<Genre> optionalGenreToUpdate = genreRepository.findById(genreId);
+
+        if (optionalGenreToUpdate.isPresent()) {
+            Genre genreToUpdate = optionalGenreToUpdate.get();
+            genreToUpdate.setGenre(genreDtoToChange.getGenre());
+            genreRepository.save(genreToUpdate);
+            return true;
+        }
+
+        throw new ResourceNotFoundException("There is not a genre with such an id");
+    }
+
+    public Genre deleteGenre(
+        @NotNull(message = "The provided genre id cannot be null")
+        @Positive(message = "The provided genre id must be positive")
+        Long genreToDeleteId) {
+
+        Optional<Genre> optionalGenreToDelete = genreRepository.findById(genreToDeleteId);
+
+        if (optionalGenreToDelete.isPresent()) {
+            Genre genreToDelete = optionalGenreToDelete.get();
+            genreRepository.delete(genreToDelete);
+            return genreToDelete;
+        }
+
+        throw new ResourceNotFoundException("There is not a genre with such an id");
+    }
+}
