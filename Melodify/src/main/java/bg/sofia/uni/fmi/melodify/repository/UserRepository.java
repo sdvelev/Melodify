cc404f9401d3b29367e9445419dc3cf7008b7859
Daniel Halachev <dani22h09_pl@abv.bg>
2023-12-28 20:07:56 +0200

    Improved REST API filter functionality.

--- Melodify/src/main/java/bg/sofia/uni/fmi/melodify/repository/UserRepository.java
+++ Melodify/src/main/java/bg/sofia/uni/fmi/melodify/repository/UserRepository.java
@@ -4,7 +4,6 @@ import bg.sofia.uni.fmi.melodify.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
